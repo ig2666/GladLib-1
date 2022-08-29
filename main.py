11dbff067bdad34e3bbb70a18f1d87071ad1a400
ig2666 <112423102+ig2666@users.noreply.github.com>
2022-08-29 13:04:05 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# GladLib-1
+GladLib has its first short adventure right here! We promise to always have happy endings. Always.
+print("Welcome to the GladLib portal! All stories have happy endings in GladLibs. Please \
+begin by following the instructions below.")
+adj1 = input("Please type in an adjective.")
+adj2 = input("Please type in another adjective.")
+number1 = int(input("Please type in a number."))
+number2 = int(input("Please type in another number."))
+noun1 = input("Type in a noun.")
+pastvb1 = input("Please type in a past tense verb.")
+pastvb2 = input("Please type in another past tense verb.")
+adv1 = input("Please type in an adverb.")
+adv2 = input("Please type in another adverb.")
+place1 = input("Please type in a place or location.")
+
+gladlib = (f"One day, a/an {adj1} dog discovered {number1} golden {noun1}(s). He {pastvb1} for joy! \
+He was so {adj2} that he {adv1} {pastvb2} it. He {pastvb2} for {number2} dollar(s). He became the \
+richest dog in {place1}. He lived {adv2} ever after.")
+print(gladlib)
